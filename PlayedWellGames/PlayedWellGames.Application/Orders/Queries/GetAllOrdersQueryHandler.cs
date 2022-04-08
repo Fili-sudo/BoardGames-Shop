3440362dfe06322f7cce1425dd8ba00851bc017b
David Filimon <davidfilimon2000@yahoo.com>
2022-04-08 10:25:56 +0300

    made a query

---
+++ PlayedWellGames/PlayedWellGames.Application/Orders/Queries/GetAllOrdersQueryHandler.cs
@@ -0,0 +1,24 @@
+﻿using MediatR;
+using PlayedWellGames.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayedWellGames.Application.Orders.Queries
+{
+    public class GetAllOrdersQueryHandler : IRequestHandler<GetAllOrdersQuery, List<Order>>
+    {
+        private IOrderRepository _orderRepository;
+        public GetAllOrdersQueryHandler(IOrderRepository _orderRepository)
+        {
+            _orderRepository = _orderRepository;
+        }
+
+        public Task<List<Order>> Handle(GetAllOrdersQuery request, CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
