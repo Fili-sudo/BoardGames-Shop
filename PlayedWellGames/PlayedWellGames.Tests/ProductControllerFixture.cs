8dba05ce0930942b7543025a08f1208345325b4a
David Filimon <davidfilimon2000@yahoo.com>
2022-04-21 15:21:48 +0300

    Added 3 more tests for ProductsController

--- PlayedWellGames/PlayedWellGames.Tests/ProductControllerFixture.cs
+++ PlayedWellGames/PlayedWellGames.Tests/ProductControllerFixture.cs
@@ -1,9 +1,14 @@
 using AutoMapper;
 using MediatR;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using PlayedWellGames.Api.Controllers;
+using PlayedWellGames.Api.Dto;
 using PlayedWellGames.Application.Products.Queries;
+using PlayedWellGames.Core;
+using System;
+using System.Net;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -28,8 +33,81 @@ namespace PlayedWellGames.Tests
             var controller = new ProductsController(_mockMediator.Object, _mockMapper.Object);
             await controller.Getall();
 
+            //Assert
             _mockMediator.Verify(x => x.Send(It.IsAny<GetAllProductsQuery>(), It.IsAny<CancellationToken>()), Times.Once());
            
+        }
+
+        [TestMethod]
+        public async Task Get_Product_By_Id_GetProductByIdQueryIsCalled()
+        {
+            //Arrange
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetProductByIdQuery>(), It.IsAny<CancellationToken>()))
+                .Verifiable();
+
+            //Act
+            var controller = new ProductsController(_mockMediator.Object, _mockMapper.Object);
+            await controller.GetById(1);
+
+            //Assert
+            _mockMediator.Verify(x => x.Send(It.IsAny<GetProductByIdQuery>(), It.IsAny<CancellationToken>()), Times.Once());
+
+        }
+
+        [TestMethod]
+        public async Task Get_Product_By_Id_GetProductByIdQueryWithCorrectProductIdIsCalled()
+        {
+            int productId = 0;
+
+            //Arrange
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetProductByIdQuery>(), It.IsAny<CancellationToken>()))
+                .Returns<GetProductByIdQuery, CancellationToken>(async (q, c) =>
+                {
+                    productId = q.Id;
+                    return await Task.FromResult(
+                       new Product
+                       {
+                           Id = q.Id,
+                           ProductName = "A product",
+                           Description = "some description",
+                           Price = 23,
+                           Quantity = 10,
+                           Tags = "some tags"
+                       });
+                    
+                });
+            _mockMapper.Setup(m => m.Map<Product, ProductGetDto>(It.IsAny<Product>())).Returns(new ProductGetDto());
+
+            //Act
+            var controller = new ProductsController(_mockMediator.Object, _mockMapper.Object);
+            var result = await controller.GetById(1);
+            
+            
+            //Assert
+            Assert.AreEqual(productId, 1);
+            
+        }
+
+        [TestMethod]
+        public async Task Get_Product_By_Id_ShouldReturnNotFound()
+        {
+
+            //Arrange
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetProductByIdQuery>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult<Product>(null));
+
+            //Act
+            var controller = new ProductsController(_mockMediator.Object, _mockMapper.Object);
+            var result = await controller.GetById(1);
+            var NotFoundResult = result as NotFoundResult;
+
+
+            //Assert
+            Assert.AreEqual((int)HttpStatusCode.NotFound, NotFoundResult.StatusCode);
+
         }
     }
 }
