15a47da7e2bc62a155f9670e98238c9eb6b0531c
David Filimon <davidfilimon2000@yahoo.com>
2022-04-29 11:18:44 +0300

    finished tests for GetUserById from UsersController

--- PlayedWellGames/PlayedWellGames.Api/Dto/UserGetDto.cs
+++ PlayedWellGames/PlayedWellGames.Api/Dto/UserGetDto.cs
@@ -12,5 +12,15 @@ namespace PlayedWellGames.Api.Dto
         public string Mail { get; set; }
         public string Phone { get; set; }
         public string Address { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || obj is not UserGetDto) return false;
+            UserGetDto other = (UserGetDto)obj;
+
+            return other.Id == Id && other.UserName == UserName && other.Role == Role 
+                && other.FirstName == FirstName && other.LastName == LastName && other.Mail == Mail
+                && other.Phone == Phone && other.Address == Address;
+        }
     }
 }
--- PlayedWellGames/PlayedWellGames.Tests/UserControllerFixture.cs
+++ PlayedWellGames/PlayedWellGames.Tests/UserControllerFixture.cs
@@ -89,5 +89,200 @@ namespace PlayedWellGames.Tests
             Assert.IsInstanceOfType(okResult.Value, typeof(List<UserGetDto>));
 
         }
+
+        [TestMethod]
+        public async Task Get_User_By_Id_GetUserByIdQueryIsCalled()
+        {
+            //Arrange
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetUserByIdQuery>(), It.IsAny<CancellationToken>()))
+                .Verifiable();
+
+            //Act
+            var controller = new UsersController(_mockMediator.Object, _mockMapper.Object);
+            await controller.GetById(1);
+
+            //Assert
+            _mockMediator.Verify(x => x.Send(It.IsAny<GetUserByIdQuery>(), It.IsAny<CancellationToken>()), Times.Once());
+
+        }
+
+        [TestMethod]
+        public async Task Get_User_By_Id_GetUserByIdQueryWithCorrectUserIdIsCalled()
+        {
+            int userId = 0;
+
+            //Arrange
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetUserByIdQuery>(), It.IsAny<CancellationToken>()))
+                .Returns<GetUserByIdQuery, CancellationToken>(async (q, c) =>
+                {
+                    userId = q.Id;
+                    return await Task.FromResult(
+                       new User
+                       {
+                           Id = q.Id,
+                           FirstName = "John",
+                           LastName = "Davis",
+                           Mail = "john.davis@gmail.com",
+                           UserName = "john.davis",
+                           Pass = "1234",
+                           Phone = "0771671642",
+                           Address = "some address",
+                           Role = Role.Regular
+                       });
+
+                });
+            _mockMapper.Setup(m => m.Map<User, UserGetDto>(It.IsAny<User>())).Returns(new UserGetDto());
+
+            //Act
+            var controller = new UsersController(_mockMediator.Object, _mockMapper.Object);
+            var result = await controller.GetById(1);
+
+
+            //Assert
+            Assert.AreEqual(userId, 1);
+
+        }
+
+        [TestMethod]
+        public async Task Get_User_By_Id_ShouldReturnNotFoundStatusCode()
+        {
+
+            //Arrange
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetUserByIdQuery>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult<User>(null));
+
+            //Act
+            var controller = new UsersController(_mockMediator.Object, _mockMapper.Object);
+            var result = await controller.GetById(1);
+            var NotFoundResult = result as NotFoundResult;
+
+
+            //Assert
+            Assert.AreEqual((int)HttpStatusCode.NotFound, NotFoundResult.StatusCode);
+
+        }
+
+        [TestMethod]
+        public async Task Get_User_By_Id_ShouldReturnOkStatusCode()
+        {
+
+            //Arrange
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetUserByIdQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new User
+                {
+                    Id = 1,
+                    FirstName = "John",
+                    LastName = "Davis",
+                    Mail = "john.davis@gmail.com",
+                    UserName = "john.davis",
+                    Pass = "1234",
+                    Phone = "0771671642",
+                    Address = "some address",
+                    Role = Role.Regular
+                });
+
+            //Act
+            var controller = new UsersController(_mockMediator.Object, _mockMapper.Object);
+            var result = await controller.GetById(1);
+            var okResult = result as OkObjectResult;
+
+
+            //Assert
+            Assert.AreEqual((int)HttpStatusCode.OK, okResult.StatusCode);
+
+        }
+
+        [TestMethod]
+        public async Task Get_User_By_Id_ShouldReturnFoundUser()
+        {
+            //Arrange
+            var user = new User
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Davis",
+                Mail = "john.davis@gmail.com",
+                UserName = "john.davis",
+                Pass = "1234",
+                Phone = "0771671642",
+                Address = "some address",
+                Role = Role.Regular
+            };
+            var userGetDto = new UserGetDto
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Davis",
+                Mail = "john.davis@gmail.com",
+                UserName = "john.davis",
+                Phone = "0771671642",
+                Address = "some address",
+                Role = Role.Regular
+            };
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetUserByIdQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(user);
+            _mockMapper
+                .Setup(m => m.Map<User, UserGetDto>(It.IsAny<User>()))
+                .Returns((User src) => new UserGetDto()
+                {
+                    Id = src.Id,
+                    FirstName = src.FirstName,
+                    LastName = src.LastName,
+                    Mail = src.Mail,
+                    UserName = src.UserName,
+                    Phone = src.Phone,
+                    Address = src.Address,
+                    Role = src.Role
+                });
+
+            //Act
+            var controller = new UsersController(_mockMediator.Object, _mockMapper.Object);
+            var result = await controller.GetById(1);
+
+            var okResult = result as OkObjectResult;
+
+            //Assert
+            Assert.AreEqual(userGetDto, okResult.Value);
+
+        }
+
+        [TestMethod]
+        public async Task Get_Product_By_Id_ResultShouldBeOfTypeProductGetDto()
+        {
+            //Arrange
+            var user = new User
+            {
+                Id = 1,
+                FirstName = "John",
+                LastName = "Davis",
+                Mail = "john.davis@gmail.com",
+                UserName = "john.davis",
+                Pass = "1234",
+                Phone = "0771671642",
+                Address = "some address",
+                Role = Role.Regular
+            };
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetUserByIdQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(user);
+            _mockMapper
+                .Setup(m => m.Map<User, UserGetDto>(It.IsAny<User>()))
+                .Returns(new UserGetDto());
+
+            //Act
+            var controller = new UsersController(_mockMediator.Object, _mockMapper.Object);
+            var result = await controller.GetById(1);
+
+            var okResult = result as OkObjectResult;
+
+            //Assert
+            Assert.IsInstanceOfType(okResult.Value, typeof(UserGetDto));
+
+        }
     }
 }
