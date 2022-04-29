db97a25359ba381d8a106ffdb20fba9df524ce76
David Filimon <davidfilimon2000@yahoo.com>
2022-04-29 12:56:04 +0300

    finished tests for GetOrderById from OrdersController

--- PlayedWellGames/PlayedWellGames.Api/Dto/OrderGetDto.cs
+++ PlayedWellGames/PlayedWellGames.Api/Dto/OrderGetDto.cs
@@ -10,5 +10,26 @@ namespace PlayedWellGames.Api.Dto
         public int UserId { get; set; }
         public List<OrderItemGetDto> OrderItems { get; set; }
         public string ShippingAddress { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj == null || obj is not OrderGetDto) return false;
+            OrderGetDto other = (OrderGetDto)obj;
+
+            bool result = true;
+
+            var array = OrderItems.ToArray();
+            var otherArray = other.OrderItems.ToArray();
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                result = result && otherArray[i].Equals(array[i]);
+                if (result == false) break;
+            }
+
+            return other.Id == Id && other.State == State && other.Price == Price && other.UserId == UserId
+                && other.ShippingAddress == ShippingAddress && result;
+
+        }
     }
 }
--- PlayedWellGames/PlayedWellGames.Tests/OrderControllerFixture.cs
+++ PlayedWellGames/PlayedWellGames.Tests/OrderControllerFixture.cs
@@ -91,5 +91,245 @@ namespace PlayedWellGames.Tests
 
         }
 
+        [TestMethod]
+        public async Task Get_Order_By_Id_GetOrderByIdQueryIsCalled()
+        {
+            //Arrange
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetOrderByIdQuery>(), It.IsAny<CancellationToken>()))
+                .Verifiable();
+
+            //Act
+            var controller = new OrdersController(_mockMediator.Object, _mockMapper.Object);
+            await controller.GetById(1);
+
+            //Assert
+            _mockMediator.Verify(x => x.Send(It.IsAny<GetOrderByIdQuery>(), It.IsAny<CancellationToken>()), Times.Once());
+
+        }
+
+        [TestMethod]
+        public async Task Get_Order_By_Id_GetOrderByIdQueryWithCorrectOrderIdIsCalled()
+        {
+            int orderId = 0;
+
+            //Arrange
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetOrderByIdQuery>(), It.IsAny<CancellationToken>()))
+                .Returns<GetOrderByIdQuery, CancellationToken>(async (q, c) =>
+                {
+                    orderId = q.Id;
+                    return await Task.FromResult(
+                       new Order
+                       {
+                           Id = q.Id,
+                       });
+
+                });
+            _mockMapper.Setup(m => m.Map<Order, OrderGetDto>(It.IsAny<Order>())).Returns(new OrderGetDto());
+
+            //Act
+            var controller = new OrdersController(_mockMediator.Object, _mockMapper.Object);
+            var result = await controller.GetById(1);
+
+
+            //Assert
+            Assert.AreEqual(orderId, 1);
+
+        }
+
+        [TestMethod]
+        public async Task Get_Order_By_Id_ShouldReturnNotFoundStatusCode()
+        {
+
+            //Arrange
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetOrderByIdQuery>(), It.IsAny<CancellationToken>()))
+                .Returns(Task.FromResult<Order>(null));
+
+            //Act
+            var controller = new OrdersController(_mockMediator.Object, _mockMapper.Object);
+            var result = await controller.GetById(1);
+            var NotFoundResult = result as NotFoundResult;
+
+
+            //Assert
+            Assert.AreEqual((int)HttpStatusCode.NotFound, NotFoundResult.StatusCode);
+
+        }
+
+        [TestMethod]
+        public async Task Get_Order_By_Id_ShouldReturnOkStatusCode()
+        {
+
+            //Arrange
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetOrderByIdQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(new Order
+                {
+                    Id = 1,
+                });
+
+            //Act
+            var controller = new OrdersController(_mockMediator.Object, _mockMapper.Object);
+            var result = await controller.GetById(1);
+            var okResult = result as OkObjectResult;
+
+
+            //Assert
+            Assert.AreEqual((int)HttpStatusCode.OK, okResult.StatusCode);
+
+        }
+
+        [TestMethod]
+        public async Task Get_Order_By_Id_ShouldReturnFoundOrder()
+        {
+            //Arrange
+            var order = new Order
+            {
+                Id = 1,
+                ShippingAddress = "some address",
+                State = States.Pending,
+                Price = 30,
+                UserId = 1,
+                OrderItems = new List<OrderItem>
+                {
+                    new OrderItem
+                    {
+                        Id = 1,
+                        OrderId = 1,
+                        ProductId = 1,
+                        Quantity = 2,
+                        Product = new Product
+                        {
+                            Id = 1,
+                            ProductName = "A product",
+                            Description = "some description",
+                            Price = 23,
+                            Quantity = 10,
+                            Tags = "some tags"
+                        }
+                    }
+                }
+            };
+            var orderGetDto = new OrderGetDto
+            {
+                Id = 1,
+                ShippingAddress = "some address",
+                State = States.Pending,
+                Price = 30,
+                UserId = 1,
+                OrderItems = new List<OrderItemGetDto>
+                {
+                    new OrderItemGetDto
+                    {
+                        Id = 1,
+                        ProductId = 1,
+                        Quantity = 2,
+                        Product = new ProductGetDto
+                        {
+                            Id = 1,
+                            ProductName = "A product",
+                            Description = "some description",
+                            Price = 23,
+                            Quantity = 10,
+                            Tags = "some tags"
+                        }
+                    }
+                }
+
+            };
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetOrderByIdQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(order);
+            _mockMapper
+                .Setup(m => m.Map<Order, OrderGetDto>(It.IsAny<Order>()))
+                .Returns((Order src) => new OrderGetDto()
+                {
+                    Id = src.Id,
+                    ShippingAddress = src.ShippingAddress,
+                    State = src.State,
+                    Price = src.Price,
+                    UserId = (int)src.UserId,
+                    OrderItems = new List<OrderItemGetDto>
+                    {
+                        new OrderItemGetDto
+                        {
+                            Id = src.OrderItems.ToArray()[0].Id,
+                            ProductId = src.OrderItems.ToArray()[0].ProductId,
+                            Quantity = src.OrderItems.ToArray()[0].Quantity,
+                            Product = new ProductGetDto
+                            {
+                                Id = src.OrderItems.ToArray()[0].Product.Id,
+                                Description = src.OrderItems.ToArray()[0].Product.Description,
+                                ProductName = src.OrderItems.ToArray()[0].Product.ProductName,
+                                Price = src.OrderItems.ToArray()[0].Product.Price,
+                                Quantity = src.OrderItems.ToArray()[0].Product.Quantity,
+                                Tags = src.OrderItems.ToArray()[0].Product.Tags
+                            }
+                        }
+                    }
+                });
+
+            //Act
+            var controller = new OrdersController(_mockMediator.Object, _mockMapper.Object);
+            var result = await controller.GetById(1);
+
+            var okResult = result as OkObjectResult;
+
+            //Assert
+            Assert.AreEqual(orderGetDto, okResult.Value);
+
+        }
+
+        [TestMethod]
+        public async Task Get_Order_By_Id_ResultShouldBeOfTypeOrderGetDto()
+        {
+            //Arrange
+            var order = new Order
+            {
+                Id = 1,
+                ShippingAddress = "some address",
+                State = States.Pending,
+                Price = 30,
+                UserId = 1,
+                OrderItems = new List<OrderItem>
+                {
+                    new OrderItem
+                    {
+                        Id = 1,
+                        OrderId = 1,
+                        ProductId = 1,
+                        Quantity = 2,
+                        Product = new Product
+                        {
+                            Id = 1,
+                            ProductName = "A product",
+                            Description = "some description",
+                            Price = 23,
+                            Quantity = 10,
+                            Tags = "some tags"
+                        }
+                    }
+                }
+            };
+            _mockMediator
+                .Setup(m => m.Send(It.IsAny<GetOrderByIdQuery>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(order);
+            _mockMapper
+                .Setup(m => m.Map<Order, OrderGetDto>(It.IsAny<Order>()))
+                .Returns(new OrderGetDto());
+
+            //Act
+            var controller = new OrdersController(_mockMediator.Object, _mockMapper.Object);
+            var result = await controller.GetById(1);
+
+            var okResult = result as OkObjectResult;
+
+            //Assert
+            Assert.IsInstanceOfType(okResult.Value, typeof(OrderGetDto));
+
+        }
+
     }
 }
