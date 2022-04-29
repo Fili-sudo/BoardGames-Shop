250875a4c189ac64dc124fdacedce16c92fcfd0d
David Filimon <davidfilimon2000@yahoo.com>
2022-04-29 15:38:52 +0300

    Added FileEntityt for pictures

---
+++ PlayedWellGames/PlayedWellGames.Core/FileEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlayedWellGames.Core
+{
+    public class FileEntity : Entity
+    {
+        public string Name { get; set; }
+        public string Extension { get; set; }
+    }
+}
