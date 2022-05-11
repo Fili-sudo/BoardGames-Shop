62456463edf52d4cdb7da9f585b1444c70881014
David Filimon <davidfilimon2000@yahoo.com>
2022-05-11 09:11:12 +0300

    added loading facility for user feedback

--- PlayedWellGames/playedwellgamesclient/src/components/ProductCards.jsx
+++ PlayedWellGames/playedwellgamesclient/src/components/ProductCards.jsx
@@ -17,6 +17,12 @@ export default function ProductCards({ products, loading }){
         {alt: "dolphin", url: "https://www.thoughtco.com/thmb/AvVf2u-2pJZC0aQD-kUlv2ESZD4=/5120x2880/smart/filters:no_upscale()/atlantic-bottlenose-dolphin--jumping-high-during-a-dolphin-training-demonstration-154724035-59ce93949abed50011352530.jpg"}
     ]);
 
+    if(loading){
+        return(
+            <h2>Loading...</h2>
+        );
+    }
+    
     return (
         <div>
             {products.map((product => (
