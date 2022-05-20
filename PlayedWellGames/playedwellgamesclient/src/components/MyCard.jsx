a5b298a06527408c76055d73213b90ad251bab0d
David Filimon <davidfilimon2000@yahoo.com>
2022-05-20 13:42:09 +0300

    added delete functionality for item card

--- PlayedWellGames/playedwellgamesclient/src/components/MyCard.jsx
+++ PlayedWellGames/playedwellgamesclient/src/components/MyCard.jsx
@@ -3,54 +3,76 @@ import { Typography } from '@mui/material';
 import { useEffect, useState } from "react";
 import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import ClearIcon from '@mui/icons-material/Clear';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
 import IconButton from '@mui/material/IconButton';
+
 
 export default function MyCard(props){
 
     const [price, setPrice] = useState(props.price);
     const [dsrQuantity, setDsrQuantity] = useState(props.desiredQuantity);
+    const [display, setDisplay] = useState(true);
 
     const updateAmount = (in_dec) =>{
-        if(dsrQuantity + in_dec != 0 && dsrQuantity + in_dec != props.quantity){
+        if(dsrQuantity + in_dec != 0 && dsrQuantity + in_dec <= props.quantity){
             setDsrQuantity(dsrQuantity + in_dec);
         }
       }
+    const deleteItem = () =>{
+        setDisplay(false);
+    }
+
+    const CardContainer = {
+        padding: "20px",
+        margin: "20px", 
+        width: "50%", 
+        display: "flex",
+        boxShadow: "3px 3px 10px 0 black", 
+        borderRadius:"10px"
+    }
 
     return(
-        <div style={{padding: "20px",boxShadow: "3px 3px 10px 0 black", borderRadius:"10px", margin: "20px", width: "50%", display: "flex"}}>
-            <div style={{display: "inline-block", maxHeight: "150px", width: "50%"}}>
-              <img src={props.image} 
-                   alt="image"
-                   style={{ width:"100%",
-                            height:"100%",
-                            objectFit:"contain",
-                          }}
-              />
-            </div>
-            <div style={{flex: "1", textAlign: "right"}}>
-              <Typography variant="h4" >
-                {props.productName}
-              </Typography>
-              <Typography variant="h5" sx={{display: "inline", margin: "5px"}}>
-                Desired quantity: {' '}{dsrQuantity}
-              </Typography>
-              <ButtonGroup size="small" variant="contained" aria-label="outlined primary button group" orientation="vertical" >
-                <IconButton onClick={(event) => updateAmount(1)}>
-                  <KeyboardArrowUpIcon/>
-                </IconButton>
-                <IconButton onClick={(event) => updateAmount(-1)}>
-                  <KeyboardArrowDownIcon/>
-                </IconButton>
-              </ButtonGroup>
-              <Typography variant="h5">
-                Price: {' '}{price*dsrQuantity}{'\u20AC'}
-              </Typography>
-              
-              
-            </div>
-            
-        </div>
+        <>
+            {display ?
+                <div style={CardContainer}>
+                    <div style={{display: "inline-block", maxHeight: "150px", width: "50%"}}>
+                      <img src={props.image} 
+                           alt="image"
+                           style={{ width:"100%",
+                                    height:"100%",
+                                    objectFit:"contain",
+                                  }}
+                      />
+                    </div>
+                    <div style={{flex: "1", textAlign: "right"}}>
+                      <Typography variant="h4" >
+                        {props.productName}
+                      </Typography>
+                              
+                      <Typography variant="h5" sx={{display: "inline", margin: "5px"}}>
+                        Desired quantity: {' '}{dsrQuantity}
+                      </Typography>
+                      <ButtonGroup size="small" variant="contained" aria-label="outlined primary button group" orientation="vertical" >
+                        <IconButton onClick={(event) => updateAmount(1)}>
+                          <KeyboardArrowUpIcon/>
+                        </IconButton>
+                        <IconButton onClick={(event) => updateAmount(-1)}>
+                          <KeyboardArrowDownIcon/>
+                        </IconButton>
+                      </ButtonGroup>
+                      <Typography variant="h5">
+                        Price: {' '}{price*dsrQuantity}{'\u20AC'}
+                      </Typography>
+                      <IconButton onClick={(event) => deleteItem()}>
+                          <ClearIcon/>
+                      </IconButton>
+                              
+                    </div>
+                              
+                </div>
+            : <></>}
+        </>
     )
 }
--- PlayedWellGames/playedwellgamesclient/src/pages/ShoppingCartComponent.jsx
+++ PlayedWellGames/playedwellgamesclient/src/pages/ShoppingCartComponent.jsx
@@ -1,7 +1,8 @@
 import * as React from 'react';
+import MyCard from "../components/MyCard"
 
 export default function ShoppingCartComponent(){
 
 
-    
+
 }
