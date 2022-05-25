7cde4bf2a40fd6c909db67a33c7e423d12fd0551
David Filimon <davidfilimon2000@yahoo.com>
2022-05-25 16:17:33 +0300

    an admin can now see a table of orders

--- PlayedWellGames/playedwellgamesclient/src/pages/OrdersPageComponent.jsx
+++ PlayedWellGames/playedwellgamesclient/src/pages/OrdersPageComponent.jsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import { useEffect, useState } from "react";
 import axios from 'axios';
+import { Outlet  } from "react-router-dom";
+import EnhancedTable from "../components/TableForOrdersComponent"
 
-export default function OrdersPageComponent(){
+export default function ProductsPageComponent(){
 
     return (
         <div>
-            <h1>Orders Page</h1>
+            <h1>Products Page</h1>
+            <div style={{padding: "20px"}}>
+                <EnhancedTable/>
+            </div>
         </div>
     );
 }
